8b3bd425616851cbc529ebd4270e6856cddb4049
m1k0 <acme@gmx.com>
2018-02-26 03:35:40 -0500

    updated method signature to use IActionResult instad of JsonResponse

---
+++ CityInfoApi/Controllers/CityController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoApi.Controllers
+{
+    [Route("api/city")]
+    public class CityController : Controller
+    {
+        //[HttpGet("api/cities")]
+        [HttpGet("")]
+        public IActionResult GetCities()
+        {
+            //var result = new JsonResult(
+            //    //new List<object>(){
+            //    //new { id =1 , name = "Skopje"},
+            //    //new { id = 2, name = "Oakville"}
+            //    //}
+
+            //    CitiesDataStore.Current.Cities
+            //);
+
+
+            var result = CitiesDataStore.Current.Cities;
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.SingleOrDefault(c => c.Id == id);
+                
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+
+
+
+
+    }
+}
