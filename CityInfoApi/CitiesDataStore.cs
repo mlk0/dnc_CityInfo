af1cbaa68a7d0ac774d929731bad67b58ac6516a
m1k0 <acme@gmx.com>
2018-02-26 02:44:53 -0500

    adding CitiesDataStore

---
+++ CityInfoApi/CitiesDataStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using CityInfoApi.Model;
+
+namespace CityInfoApi
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities;
+
+        public CitiesDataStore()
+        {
+            this.Cities = new List<CityDto>()
+            {
+                new CityDto(){
+                    Id = 1,
+                    Name = "Skopje",
+                    Description = "Najubav na svet",
+                    NumberOfPointsOfInteres = 7
+                },
+                new CityDto(){
+                    Id = 2,
+                    Name = "Ohrid",
+                    Description = "Nasakan na svet",
+                    NumberOfPointsOfInteres = 17
+                },
+                new CityDto(){
+                    Id = 3,
+                    Name = "Kocani",
+                    Description = "Najvesel na svet",
+                    NumberOfPointsOfInteres = 12
+                }
+            };
+        }
+    }
+}
--- CityInfoApi/CityController.cs
+++ CityInfoApi/CityController.cs
@@ -12,10 +12,12 @@ namespace CityInfoApi
         public JsonResult GetCities()
         {
             return new JsonResult(
-                new List<object>(){
-                new { id =1 , name = "Skopje"},
-                new { id = 2, name = "Oakville"}
-            }
+                //new List<object>(){
+                //new { id =1 , name = "Skopje"},
+                //new { id = 2, name = "Oakville"}
+                //}
+
+                CitiesDataStore.Current.Cities
             );
         }
 
